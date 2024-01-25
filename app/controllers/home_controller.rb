134ce5cc672e154ac4b96a82b30d60bbb0d1ce01
Solam <solamnyangiwe@gmail.com>
2024-01-25 15:13:20 +0200

    root controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   # Your actions go here
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   def index
     # Your code for the index action goes here
   end
