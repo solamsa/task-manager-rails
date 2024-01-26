5cdfe4374e0277d9a4ee76f16c22e59a13b1e201
Solam <solamnyangiwe@gmail.com>
2024-01-26 15:50:32 +0200

    user kaminari

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,2 +1,16 @@
 require "rails_helper"
 
+RSpec.describe User, type: :model do
+  let(:user) { create(:user) }
+
+  it "has a valid factory" do
+    expect(user).to be_valid 
+  end
+
+  context 'Should not be valid' do
+    it 'when email is not present' do
+      user.email = nil
+      expect(user).not_to be_valid
+    end
+  end
+end
