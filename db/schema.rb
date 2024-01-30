4b43436b9addb5186079b3c508fbebee47c8a099
Solam <solamnyangiwe@gmail.com>
2024-01-30 14:36:56 +0200

    priority schema support

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_24_094245) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_30_112652) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_24_094245) do
     t.uuid "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "priority"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
