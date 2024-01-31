124c50202a7c298fb13ff44d4e1d90a09be099f3
Solam <solamnyangiwe@gmail.com>
2024-01-31 17:20:43 +0200

    task model with hash

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
+  enum status: { todo: 0, inprogress: 1, complete: 2 }
   belongs_to :user
-  
+
   validates :title, presence: true
   validates :description, presence: true, length: { minimum: 10 }
 
