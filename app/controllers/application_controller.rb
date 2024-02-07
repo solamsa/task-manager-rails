39a7c9d27c4c5c22a6ba1f114ec47d262c194676
Solam <solamnyangiwe@gmail.com>
2024-02-07 14:32:32 +0200

    app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
 
   before_action :authenticate_user!
 
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  
+  protected
+
+  def configure_permitted_parameters
+       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name,:last_name, :email,:password_confirmation, :password)}
+
+       devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name,:last_name, :email, :current_password)}
+  end
 
 end
