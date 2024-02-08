cb697fcae81da9eb38e3b9f5f90517f6e762178a
Solam <solamnyangiwe@gmail.com>
2024-02-08 13:22:13 +0200

    show devise route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  devise_scope :user do
+    get 'users/registrations/show', to: 'users/registrations#show', as: 'show_user_registration'
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -15,8 +19,6 @@ Rails.application.routes.draw do
     end
   end
   
-  # devise_for :users, controllers: { registrations: 'users/registrations' }
-  # devise_for :users
 
   get "up" => "rails/health#show", as: :rails_health_check
 
