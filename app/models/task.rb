df629e8beaa4c8d91c5bbc15aa7e83e20cecb8f3
Solam <solamnyangiwe@gmail.com>
2024-02-09 12:01:34 +0200

    task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,11 @@
 class Task < ApplicationRecord
   enum status: { todo: 0, inprogress: 1, complete: 2 }
   enum priority: { low: 0, medium: 1, high: 2 }
+
   belongs_to :user
 
   validates :title, presence: true
   validates :description, presence: true, length: { minimum: 10 }
+  validates :estimate, presence: true
 
 end
