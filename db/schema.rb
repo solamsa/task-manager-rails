73cb410650e7bd027512e39311df4d69dadd7423
Solam <solamnyangiwe@gmail.com>
2024-02-09 12:04:33 +0200

    estimate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_07_074439) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_09_074902) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_07_074439) do
     t.datetime "updated_at", null: false
     t.integer "priority"
     t.integer "status"
+    t.interval "estimate"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
