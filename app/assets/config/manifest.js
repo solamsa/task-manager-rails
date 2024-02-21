353eef85e642ba0752daa4dbcdd2a0c314b4b3bf
Solam <solamnyangiwe@gmail.com>
2024-02-21 17:08:05 +0200

    solved console error

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,9 @@
 //= link_tree ../images
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
-//= link application.css
 //= link popper.js
+//= link application.css
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
+//= link controllers/tasks_controller.js
+
