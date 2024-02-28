c166cfd325347c81c70a43f58afdcf9ac504195d
Solam <solamnyangiwe@gmail.com>
2024-02-28 09:52:47 +0200

    controllers

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -66,7 +66,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to quotes_path, notice: "Task was successfully destroyed." }
+      format.html { redirect_to tasks_path, notice: "Task was successfully destroyed." }
       format.turbo_stream
     end
 
